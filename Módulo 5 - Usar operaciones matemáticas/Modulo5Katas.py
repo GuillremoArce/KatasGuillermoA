d5536f2229f4e9152ec7f8f4d97838251a5574a2
GuillermoArce <99522617+GuillremoArce@users.noreply.github.com>
2022-02-20 00:45:41 -0600

    Adding katas

---
+++ Módulo 5 - Usar operaciones matemáticas/Modulo5Katas.py
@@ -0,0 +1,9 @@
+planeta1=input("Introduce la distancia entre el planeta #1 y el sol ")
+planeta2=input("Introduce la distancia entre el planeta #2 y el sol")
+planeta1=int(planeta1)
+planeta2=int(planeta2)
+
+distanciaPlanetas=abs(planeta1-planeta2)
+
+print("La distancia en Km es de ", distanciaPlanetas)
+print("La distancia en Mi es de ", distanciaPlanetas*0.621)
